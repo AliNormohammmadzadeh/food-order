cbf1923dab7ae9a5a6a5836b9dd1df2b0a049825
AliNormohammmadzadeh <104034996+AliNormohammmadzadeh@users.noreply.github.com>
2023-12-08 19:32:26 +0330

    change production part

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,4 @@
 export const MONGO_URI = "mongodb+srv://tebalen:akza1374@cluster0.h0welaw.mongodb.net/?retryWrites=true&w=majority"
 export const APP_SECRET = "MY_SECRET_APP_KEY"
+
+export const PORT = process.env.PORT || 8000
--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -1,14 +1,13 @@
 import express, { Application } from 'express';
-import bodyParser from 'body-parser';
-import path from "path"
 
 
 import { AdminRoute , CustomerRoute, ShoppingRoute, VandorRoute} from "../routes"
+import path from 'path';
 
 
 export default async(app : Application) => {
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended:true}))
+    app.use(express.json())
+    app.use(express.urlencoded({extended:true}))
     app.use('/images', express.static(path.join(__dirname, 'images')));
 
     app.use('/admin', AdminRoute)
